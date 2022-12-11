1b60d33562b49af909bb3711f3e7ece45ecbc135
saurabh7998 <saurabhgade7998@gmail.com>
2022-12-10 22:58:51 -0500

    changes custom endpoints to avoid conflicts with spotify api endpoints

--- users/users-controller.js
+++ users/users-controller.js
@@ -2,80 +2,82 @@ import * as userDao from './users-dao.js'
 
 // let currentUser = null
 
+
+
 const UsersController = (app) => {
+    app.get('/api/users', findAllUsers)
+    app.get('/api/users/:uid', findUserById)
+    app.post('/api/users', createUser)
+    app.put('/api/users/:uid', updateUser)
+    app.delete('/api/users/:uid', deleteUser)
 
-    const findAllUsers = async (req, res) => {
-        const users = await userDao.findAllUsers()
-        res.json(users)
-    }
-    const createUser = async (req, res) => {
-        const newUser = req.body;
-        const actualUser = await userDao.createUser(newUser)
-        res.json(actualUser)
-    }
-    const updateUser = () => {}
-    const deleteUser = () => {}
+    app.post('/api/register', register)
+    app.post('/api/login', login)
+    app.post('/api/logout', logout)
+    app.post('/api/profile', profile)
+}
 
-    const register = async (req, res) => {
-        const user = req.body;
-        const existingUser = await userDao
-            .findUserByUsername(user.username)
-        if(existingUser) {
-            res.sendStatus(403)
-            return
-        }
-        const currentUser = await userDao.createUser(user)
-        console.log(currentUser)
-        req.session['currentUser'] = currentUser
-        res.json(currentUser)
-    }
+const findAllUsers = async (req, res) => {
+    const users = await userDao.findAllUsers()
+    res.json(users)
+}
+const createUser = async (req, res) => {
+    const newUser = req.body;
+    const actualUser = await userDao.createUser(newUser)
+    res.json(actualUser)
+}
+const updateUser = () => {}
+const deleteUser = () => {}
 
-    const login = async (req, res) => {
-        const credentials = req.body
-        const existingUser = await userDao
-            .findUserByCredentials(
-                credentials.username, credentials.password)
-        if(existingUser) {
-            req.session['currentUser'] = existingUser
-            res.json(existingUser)
-            return
-        }
+const register = async (req, res) => {
+    const user = req.body;
+    const existingUser = await userDao
+        .findUserByUsername(user.username)
+    if(existingUser) {
+        res.sendStatus(403)
+        return
+    }
+    const currentUser = await userDao.createUser(user)
+    console.log(currentUser)
+    req.session['currentUser'] = currentUser
+    res.json(currentUser)
+}
+
+const login = async (req, res) => {
+    const credentials = req.body
+    const existingUser = await userDao
+        .findUserByCredentials(
+            credentials.username, credentials.password)
+    if(existingUser) {
+        req.session['currentUser'] = existingUser
+        res.json(existingUser)
+        return
+    }
+    res.sendStatus(403)
+}
+
+const logout = (req, res) => {
+    req.session.destroy()
+    res.sendStatus(200)
+}
+
+const profile = (req, res) => {
+    if (req.session['currentUser']) {
+        res.send(req.session['currentUser'])
+    } else {
         res.sendStatus(403)
     }
-
-    const logout = (req, res) => {
-        req.session.destroy()
-        res.sendStatus(200)
-    }
-
-    const profile = (req, res) => {
-        if (req.session['currentUser']) {
-            res.send(req.session['currentUser'])
-        } else {
-            res.sendStatus(403)
-        }
-    }
-
-    const findUserById = async (req, res) => {
-        const uid = req.params.uid
-        const user = await userDao.findUserById(uid)
-        if (user) {
-            res.json(user)
-            return
-        }
-        res.sendStatus(404)
-    }
-
-    app.get('/users', findAllUsers)
-    app.get('/users/:uid', findUserById)
-    app.post('/users', createUser)
-    app.put('/users/:uid', updateUser)
-    app.delete('/users/:uid', deleteUser)
-
-    app.post('/register', register)
-    app.post('/login', login)
-    app.post('/logout', logout)
-    app.post('/profile', profile)
 }
+
+const findUserById = async (req, res) => {
+    const uid = req.params.uid
+    const user = await userDao.findUserById(uid)
+    if (user) {
+        res.json(user)
+        return
+    }
+    res.sendStatus(404)
+}
+
 
 export default UsersController
